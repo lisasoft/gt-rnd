8060d612b7eff0290f612ea14f0f22621a194c52
Jody Garnett <jody.garnett@gmail.com>
2011-07-11 19:53:56 +1000

    Start setting up to wrarp features
    Setting up to wrap features; been trying to keep all Feature stuff in the FaceDatastore leaving the DAO "pure". Wish I called it POJODataStore and made it reusable.

--- prototype/face/src/main/java/com/lisasoft/face/data/FaceDataStore.java
+++ prototype/face/src/main/java/com/lisasoft/face/data/FaceDataStore.java
@@ -13,35 +13,63 @@
  */
 package com.lisasoft.face.data;
 
+import java.beans.BeanInfo;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.geotools.data.Query;
 import org.geotools.data.store.ContentDataStore;
 import org.geotools.data.store.ContentEntry;
 import org.geotools.data.store.ContentFeatureSource;
+import org.geotools.feature.NameImpl;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.Name;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * This is a really simple DataStore that is built around a List<Face>.
  * <p>
  * It provides wrappers making the contents available as Feature for use the rendering engine.
  * <p>
- * The first draft is read-only; as such we do not need to worry about transactions or
- * modifications to the wrapped objects.
+ * The first draft is read-only; as such we do not need to worry about transactions or modifications
+ * to the wrapped objects.
  * 
  * @author Jody Garnett
  */
 public class FaceDataStore extends ContentDataStore {
 
-    @Override
+    private FaceDAO data;
+
+    public FaceDataStore(FaceDAO data) {
+        this.data = data;
+    }
+
     protected List<Name> createTypeNames() throws IOException {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.singletonList((Name) new NameImpl("Face"));
     }
 
     @Override
     protected ContentFeatureSource createFeatureSource(ContentEntry entry) throws IOException {
-        // TODO Auto-generated method stub
+        return new FaceFeatureSource(entry, Query.ALL);
+    }
+
+    public FaceDAO getData() {
+        return data;
+    }
+
+    public SimpleFeature toFeature(SimpleFeatureType schema, Face face) {
+        BeanInfo info = getData().getBeanInfo();
+        
+        SimpleFeatureBuilder build = new SimpleFeatureBuilder(schema);
+        
         return null;
     }
 
---
+++ prototype/face/src/main/java/com/lisasoft/face/data/FaceFeatureReader.java
@@ -0,0 +1,71 @@
+package com.lisasoft.face.data;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.geotools.data.FeatureReader;
+import org.geotools.data.store.ContentState;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+public class FaceFeatureReader implements FeatureReader<SimpleFeatureType, SimpleFeature> {
+
+    private ContentState state;
+    private Iterator<Face> iterator;
+
+    Face next; // next face
+    Face face; // current face
+    
+    public FaceFeatureReader(ContentState state) {
+        this.state = state;
+        FaceDataStore dataStore = (FaceDataStore) state.getEntry().getDataStore();
+        
+        // we grab a threadsafe copy on write list here to represent reading        
+        CopyOnWriteArrayList<Face> contents = dataStore.getData().contents();
+        iterator = contents.iterator();        
+    }
+
+    FaceDataStore getDataStore(){
+        return (FaceDataStore) state.getEntry().getDataStore();
+    }
+    
+    public SimpleFeatureType getFeatureType() {
+        return state.getFeatureType();
+    }
+
+    public SimpleFeature next() throws IOException, IllegalArgumentException,
+            NoSuchElementException {
+        if( next != null ){
+            face = next;
+            next = null;
+        }
+        else {
+            face = nextFace();
+        }
+        SimpleFeature feature = getDataStore().toFeature( getFeatureType(), face );
+        return feature;
+    }
+
+    public boolean hasNext() throws IOException {
+        if( next != null ){
+            return true;
+        }
+        else {
+            next = nextFace();
+            return next != null;
+        }
+    }
+    
+    private Face nextFace(){
+        return iterator == null ? null : iterator.next();
+    }
+
+    public void close() throws IOException {
+        iterator = null;
+        next = null;
+        face = null;
+    }
+
+}
